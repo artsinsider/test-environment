2c8cbe31d8e59fff77ffe9512ac3462069826c2e
Диденко Егор <didenko_e@tass.ru>
2018-02-02 16:39:42 +0300

    Add history and link router

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import React, { Component } from 'react';
 import Root from './components/Root/Root'
 import store from './redux'
 import {Provider} from 'react-redux'
+import {ConnectedRouter} from 'react-router-redux'
+import history from './history'
 
 class App extends Component {
   render() {
     return (
       <Provider store={store}>
+        <ConnectedRouter history={history} >
           <Root/>
+        </ConnectedRouter>
       </Provider>
     );
   }
--- src/redux/index.js
+++ src/redux/index.js
@@ -1,7 +1,10 @@
-import {createStore} from 'redux'
-import reducer from './reducer'
+import {createStore, applyMiddleware} from 'redux'
+import reducer                        from './reducer'
+import {logger}                       from 'redux-logger'
+import thunk                          from 'redux-thunk'
 
-const store = createStore(reducer);
+const enhancer = applyMiddleware(logger, thunk);
+const store = createStore(reducer, enhancer);
 
 window.store = store;
 
